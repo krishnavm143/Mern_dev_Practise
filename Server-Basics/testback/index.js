5d3721083c35299c64e7dabb197f2a7a2a4832ad
krishna <krishnavm143@gmail.com>
2023-01-30 08:40:30 +0530

    database connectivity and middelware

--- Server-Basics/testback/index.js
+++ Server-Basics/testback/index.js
@@ -24,10 +24,20 @@ app.get("/krishna", (req, res) => {
   return res.send("hey all in instagaram");
 });
 
+const isAdmin=(req,res,next)=>{
+  console.log('isadmin')
+  next()
+}
+
+const isLoggedIn=(req,res,next)=>{
+  console.log('isLoggedin')
+  next()
+}
+
 const admin = (req, res) => {
   return res.send("This is admin Page");
 };
-app.get("/admin", admin);
+app.get("/admin",isLoggedIn,isAdmin, admin);
 app.listen(port, () => {
   console.log(`server is running at port: ${port}`);
 });
--- lcomernbootcamp/projbackend/app.js
+++ lcomernbootcamp/projbackend/app.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const app = express();
+
 mongoose
   .connect(process.env.DATABASEURL, {
     useUnifiedTopology: true,
@@ -12,8 +16,15 @@ mongoose
     console.log("Database connected", process.env.DATABASEURL);
   });
 
-const port = 8000;
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors())
 
+const port = process.env.PORT || 8000;
+
+app.get('/',(req,res)=>{
+  return res.send('Hello Mongodb')
+})
 app.listen(port, () => {
   console.log(`App is listening at ${port}`);
 });
