5bd4a104c9fba858d5a5d761f47fe29f04d7ee2c
krishna <krishnavm143@gmail.com>
2023-01-31 01:08:37 +0530

    middlerware

--- lcomernbootcamp/projbackend/controllers/authentication.js
+++ lcomernbootcamp/projbackend/controllers/authentication.js
@@ -1,71 +1,95 @@
-const User=require("../models/user")
-const {validationResult}=require('express-validator')
-var jwt = require('jsonwebtoken');
-var expressJwt  = require("express-jwt");
+const User = require("../models/user");
+const { validationResult } = require("express-validator");
+var jwt = require("jsonwebtoken");
+var expressJwt = require("express-jwt");
 
-
-
-
-
-exports.signup=(req,res)=>{
-    const errors=validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            errors:errors.array()
-        })
+exports.signup = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "Not able to save in DB",
+      });
     }
-    const user=new User(req.body)
-    user.save((err,user)=>{
-        if(err){
-            return res.status(400).json({
-                err:"Not able to save in DB"
-            })
-        }
-        res.json({
-            name:user.name,
-            id:user._id,
-            email:user.email
-        })
-    })
-}
-
-
-exports.signin=(req,res)=>{
-    const {email,password}=req.body
-
-    const errors=validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            errors:errors.array()
-        })
-    }
-    User.findOne({email},(err,user)=>{
-        if(err || !user){
-            return res.status(400).json({
-                err:"user email does'nt exist"
-            })
-        }
-        if(!user.authenticate(password)){
-            return res.json({
-                err:"password do not match"
-            })
-        }
-        // Create a token
-        const token=jwt.sign({_id:user._id},process.env.SECRET)
-        // put token in cookie
-        res.cookie("token",token,{expire:new Date()+ 99999})
-
-        // send res to front end
-        const {_id,name,email,role}=user;
-       return  res.json({
-            token,user:{_id,name,email,role}
-        })
-    })
-}
-
-exports.signout=(req,res)=>{
-    res.clearCookie("token")
     res.json({
-        message:'User signout successfully'
-    })
-}
+      name: user.name,
+      id: user._id,
+      email: user.email,
+    });
+  });
+};
+
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        err: "user email does'nt exist",
+      });
+    }
+    if (!user.authenticate(password)) {
+      return res.json({
+        err: "password do not match",
+      });
+    }
+    // Create a token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    // put token in cookie
+    res.cookie("token", token, { expire: new Date() + 99999 });
+
+    // send res to front end
+    const { _id, name, email, role } = user;
+    return res.json({
+      token,
+      user: { _id, name, email, role },
+    });
+  });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("token");
+  res.json({
+    message: "User signout successfully",
+  });
+};
+
+// protected route
+
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty: "auth",
+});
+
+// custom middelware
+
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+  if (!checker) {
+    res.status(403).json({
+      error: "Acces Denied",
+    });
+  }
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    res.status(403).json({
+      error: "You are not admin",
+    });
+  }
+  next();
+};
--- lcomernbootcamp/projbackend/routes/authentication.js
+++ lcomernbootcamp/projbackend/routes/authentication.js
@@ -1,21 +1,33 @@
-const express=require('express')
-const router=express.Router();
-const { check,body,validationResult } = require('express-validator');
+const express = require("express");
+const router = express.Router();
+const { check, body, validationResult } = require("express-validator");
 
-const {signout,signup,signin}=require('../controllers/authentication')
+const {
+  signout,
+  signup,
+  signin,
+  isSignedIn,
+} = require("../controllers/authentication");
 
+router.post(
+  "/signup",
+  body("name", "min 3").isLength({ min: 3 }),
+  body("email", "email field is required").isEmail(),
+  body("password", "password should be min 5").isLength({ min: 5 }),
+  signup
+);
 
-router.post('/signup',
-body('name',"min 3").isLength({min:3}),
-body('email',"email field is required").isEmail(),
-body('password',"password should be min 5").isLength({min:5})
-,signup)
+router.post(
+  "/signin",
+  body("email", "email field is required").isEmail(),
+  body("password", "password filed is required").isLength({ min: 1 }),
+  signin
+);
 
-router.post('/signin',
-body('email',"email field is required").isEmail(),
-body('password',"password filed is required").isLength({min:1})
-,signin)
+router.get("/signout", signout);
 
-router.get('/signout',signout)
+router.get("/testRoutes", isSignedIn, (req, res) => {
+  res.json(req.auth);
+});
 
-module.exports=router;
+module.exports = router;
